78b187e6124a466bdb3191355a962716a9b7a12f
alus1337 <jazvburks95627@gmail.com>
2025-08-24 11:31:14 -0700

    Working on add sub button

--- scripts/subs.js
+++ scripts/subs.js
@@ -3,6 +3,7 @@ import {
   collection,
   getDoc,
   getDocs,
+  setDoc,
   doc,
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
@@ -12,6 +13,25 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 let userId;
+
+function notify(message) {
+  const container = document.getElementById("notification-container");
+
+  if (container) {
+    const notification = document.createElement("div");
+    notification.classList.add("notification");
+    notification.textContent = message;
+    container.appendChild(notification);
+
+    setTimeout(() => {
+      notification.classList.add("show");
+    }, 60);
+
+    setTimeout(() => {
+      notification.classList.remove(show); //finish this
+    });
+  }
+}
 
 onAuthStateChanged(auth, async (user) => {
   if (user) {
@@ -48,7 +68,7 @@ onAuthStateChanged(auth, async (user) => {
 
 const addButton = document.getElementById("add-content");
 if (addButton) {
-  addButton.addEventListener("click", () => {
+  addButton.addEventListener("click", async () => {
     const body = document.body;
 
     body.insertAdjacentHTML(
@@ -59,22 +79,22 @@ if (addButton) {
         <h2>Add New Subscription</h2>
         <label class="input-container">
           Service Name
-          <input type="text" placeholder="Enter service name" />
+          <input type="text" placeholder="Enter service name" id="add-service"/>
         </label>
 
         <label class="input-container">
           Payment Amount
-          <input type="text" />
+          <input type="text" id="add-amount"/>
         </label>
 
         <div class="frequency-container">
-          <p>Pyament Frequency</p>
+          <p>Payment Frequency</p>
           <label class="frequency-selection">
-            <input type="checkbox" />
+            <input type="checkbox" id="add-frequency-monthly"/>
             Monthly
           </label>
 
-          <label class="frequency-selection">
+          <label class="frequency-selection" id="add-frequency-yearly">
             <input type="checkbox" />
             Yearly
           </label>
@@ -82,7 +102,7 @@ if (addButton) {
 
         <label class="due-container">
           Next Payment Date
-          <input type="text" placeholder="YYYY-MM-DD" />
+          <input type="text" placeholder="YYYY-MM-DD" id="add-due"/>
         </label>
 
         <button id="add-submit">Submit</button>
@@ -99,6 +119,49 @@ if (addButton) {
         if (overlay) {
           overlay.remove();
         }
+      });
+    }
+
+    const submitButton = document.getElementById("add-submit");
+    if (submitButton) {
+      submitButton.addEventListener("click", async () => {
+        const serviceName = document.getElementById("add-service");
+        const paymentAmount = document.getElementById("add-amount");
+        let frequencyMonthly = document.getElementById("add-frequency-monthly");
+        let frequencyYearly = document.getElementById("add-frequency-yearly");
+        const nextPayDate = document.getElementById("add-due");
+
+        if (
+          !serviceName ||
+          !paymentAmount ||
+          !frequencyMonthly ||
+          !nextPayDate
+        ) {
+          console.log("One or more required elements are missing");
+          return;
+        }
+
+        if (paymentAmount.value[0] != "$") {
+          paymentAmount.value = "$" + paymentAmount.value;
+        }
+
+        if (!frequencyMonthly.checked && !frequencyYearly.checked) {
+          notify("A duration must be selected!");
+          return;
+        }
+
+        if (frequencyMonthly.checked) {
+          frequencyMonthly = "Monthly";
+        } else {
+          frequencyMonthly = "Yearly";
+        }
+
+        await setDoc(doc(db, "users", userId, "services", serviceName.value), {
+          Amount: paymentAmount.value,
+          Due: nextPayDate.value,
+          Frequency: frequencyMonthly,
+          Service: serviceName.value,
+        });
       });
     }
   });
